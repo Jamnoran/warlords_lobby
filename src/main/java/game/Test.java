d313dfcdf580022f685b56bcafbfadadeb8e6522
Jamnoran <lindahl.eric@gmail.com>
2017-05-25 14:35:43 +0200

    Local changes

--- src/main/java/game/Test.java
+++ src/main/java/game/Test.java
@@ -1,14 +1,20 @@
 package game;
 
+import game.logging.Log;
+import game.util.DatabaseUtil;
+import game.vo.Hero;
+
 public class Test {
 
 	private static final String TAG = Test.class.getSimpleName();
 
 	public static void main(String[] args) {
-		String json = "{\"request_type\":\"CLIENT_TYPE\"}";
-		System.out.println("Text: " + json);
-		String responseTypeString = "\"request_type\":\"";
-		String newJson = json.substring(json.indexOf(responseTypeString) + responseTypeString.length());
-		System.out.println(newJson.substring(0, newJson.indexOf("\"")));
+		//String json = "{\"request_type\":\"CLIENT_TYPE\"}";
+		//System.out.println("Text: " + json);
+		//String responseTypeString = "\"request_type\":\"";
+		//String newJson = json.substring(json.indexOf(responseTypeString) + responseTypeString.length());
+		//System.out.println(newJson.substring(0, newJson.indexOf("\"")));
+		Hero hero = DatabaseUtil.getHero(16);
+		Log.i(TAG, "Hero : " + hero.toString());
 	}
 }
